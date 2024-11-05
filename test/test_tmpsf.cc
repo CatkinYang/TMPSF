780ad28e429b7b1d15d3b2a3b7037d6eec1aed56
catkin <yy995332929@163.com>
2024-11-05 19:09:55 +0800

    [feat]: modify the task.json

--- test/test_tmpsf.cc
+++ test/test_tmpsf.cc
@@ -8,9 +8,9 @@
 
 namespace TMPSF {
 
-TEST(TMPSF, BASIC_TEST1) {
+TEST(TMPSF, T10_1) {
     auto tm = std::make_shared<TaskManager>();
-    tm->init_from_json("task.json");
+    tm->init_from_json("t10_1.json");
     auto fpga = std::make_shared<FPGA>("vc707");
     fpga->initFromFile("vc707");
 
@@ -23,7 +23,43 @@ TEST(TMPSF, BASIC_TEST1) {
         std::cout << "Task " << id << std::endl;
         task->printShape();
     }
-    tmpsf->taskFullInfoToJsonFile("vc707_task_json");
+    tmpsf->taskFullInfoToJsonFile("vc707_t10_1");
+}
+
+TEST(TMPSF, T10_2) {
+    auto tm = std::make_shared<TaskManager>();
+    tm->init_from_json("t10_2.json");
+    auto fpga = std::make_shared<FPGA>("vc707");
+    fpga->initFromFile("vc707");
+
+    auto tmpsf = std::make_unique<TMPSF>(tm, fpga);
+    std::cout << "******************START********************" << std::endl;
+    tmpsf->taskShapeGenerator();
+    std::cout << "*******************END*********************" << std::endl;
+
+    for (const auto &[id, task] : tm->getTask()) {
+        std::cout << "Task " << id << std::endl;
+        task->printShape();
+    }
+    tmpsf->taskFullInfoToJsonFile("vc707_t10_2");
+}
+
+TEST(TMPSF, T10_3) {
+    auto tm = std::make_shared<TaskManager>();
+    tm->init_from_json("t10_3.json");
+    auto fpga = std::make_shared<FPGA>("vc707");
+    fpga->initFromFile("vc707");
+
+    auto tmpsf = std::make_unique<TMPSF>(tm, fpga);
+    std::cout << "******************START********************" << std::endl;
+    tmpsf->taskShapeGenerator();
+    std::cout << "*******************END*********************" << std::endl;
+
+    for (const auto &[id, task] : tm->getTask()) {
+        std::cout << "Task " << id << std::endl;
+        task->printShape();
+    }
+    tmpsf->taskFullInfoToJsonFile("vc707_t10_3");
 }
 
 } // namespace TMPSF
